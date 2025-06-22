e316c451e2c357118b995b9f276735df7abf1e21
minhhuy2409 <164725462+minhhuy2409@users.noreply.github.com>
2025-06-22 10:39:28 +0700

    Add files via upload

---
+++ CNLTA-HP6-Final-VIONDRI/CNLTA-HP6-Final-VIONDRI/CNLTA_HP6_Final_VIONDRIApp.swift
@@ -0,0 +1,17 @@
+//
+//  CNLTA_HP6_Final_VIONDRIApp.swift
+//  CNLTA-HP6-Final-VIONDRI
+//
+//  Created by huy bin on 22/6/25.
+//
+
+import SwiftUI
+
+@main
+struct CNLTA_HP6_Final_VIONDRIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CNLTA-HP6-Final-VIONDRI/CNLTA-HP6-Final-VIONDRI/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CNLTA-HP6-Final-VIONDRI
+//
+//  Created by huy bin on 22/6/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
